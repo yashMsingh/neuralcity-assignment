303aa805c80d9fd0578f884f0baf710470ad6e44
Yash Singh <158991378+yashMsingh@users.noreply.github.com>
2026-06-06 16:37:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: 'neuralcity-assignment',
+})
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
